994b4aff30a0fe3501c30c5b7c23c4ad6c2b07bb
Maged Ahmed <maged.ahmed10@hotmail.com>
2019-10-10 21:44:50 +0200

    Create config.Example.php

---
+++ config.Example.php
@@ -0,0 +1,15 @@
+<?php
+
+
+return [
+  'database' => [
+      'name' => 'dbname',
+      'username' => 'root',
+      'password' => 'ENTER-YOUR-PASSWORD',
+      'connection' => 'mysql:host=localhost',
+      'options' => [
+          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+      ]
+  ]
+
+];
