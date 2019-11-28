34e9915d6b3f75f227ac552bd311029aaddf889d
MagedAhmad <maged.ahmed10@hotmail.com>
2019-11-28 21:30:44 +0200

    helper functions for hitting urls

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -19,28 +19,37 @@ class RouterTest extends TestCase
     public function test_catch_exception_when_visiting_wrong_url()
     {
         $this->expectException(RouterException::class);
-
-        $_SERVER['REQUEST_METHOD'] = 'GET';
-        $_SERVER['PATH_INFO'] = '/wrong-url';
+        $this->get('/wrong-url');
         $this->router->get();
     }
 
     public function test_catch_exception_when_method_in_controller_doesnt_exist()
     {
         $this->expectException(RouterException::class);
-
-        $_SERVER['REQUEST_METHOD'] = 'GET';
-        $_SERVER['PATH_INFO'] = '/test';
+        $this->get('/test');
         $this->router->get();
     }
 
     public function test_catch_exception_when_routes_file_is_in_wrong_format()
     {
         $this->expectException(RouterException::class);
-
-        $_SERVER['REQUEST_METHOD'] = 'GET';
-        $_SERVER['PATH_INFO'] = '/';
+        $this->get('/');
         $this->router = new Router(['/' => 'HomeController@home'], new Request);
         $this->router->get();
+    }
+
+    protected function get(string $uri)
+    {
+        $this->call($uri, 'GET');
+    }
+
+    protected function post(string $uri)
+    {
+        $this->call($uri, 'POST');
+    }
+
+    private function call(string $uri, string $methodType) {
+        $_SERVER['PATH_INFO'] = $uri;
+        $_SERVER['REQUEST_METHOD'] = $methodType;
     }
 }
